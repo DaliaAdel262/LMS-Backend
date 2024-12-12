a8dc277f28d3972ea56045a90df58892b37a289b
Dalia Adel <daliaadel262@gmail.com>
2024-12-12 20:47:20 +0200

    added default username and password

--- LMS/src/main/java/org/software_assignment/lms/LmsApplication.java
+++ LMS/src/main/java/org/software_assignment/lms/LmsApplication.java
@@ -1,5 +1,6 @@
 package org.software_assignment.lms;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LmsApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("SPRING_SECURITY_USER_NAME", dotenv.get("SPRING_SECURITY_USER_NAME"));
+		System.setProperty("SPRING_SECURITY_USER_PASSWORD", dotenv.get("SPRING_SECURITY_USER_PASSWORD"));
 		SpringApplication.run(LmsApplication.class, args);
 	}
 
