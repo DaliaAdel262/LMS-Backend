e5dbb6de8c36c089d20d543d05b2a855740c87a1
Dalia Adel <daliaadel262@gmail.com>
2024-12-14 08:40:21 +0200

    basic struct of add course

--- LMS/src/main/java/org/software_assignment/lms/controller/CourseController.java
+++ LMS/src/main/java/org/software_assignment/lms/controller/CourseController.java
@@ -1,4 +1,30 @@
 package org.software_assignment.lms.controller;
+import org.software_assignment.lms.entity.CourseEntity;
+import org.software_assignment.lms.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api/courses")
 public class CourseController {
+
+//    @Autowired
+//    private CourseService courseService;
+//
+//    @PostMapping
+//    public ResponseEntity<CourseEntity> addCourse(
+//            @RequestBody CourseEntity course, // validation must be added, ask entity to add validation
+//            @RequestHeader("role") String userRole // Assume role is passed in request headers
+//            // we can use spring security access control for this too
+//    ) {
+//        // Verify the user's role in the controller
+//        if (!"Admin".equalsIgnoreCase(userRole)) {
+//            return ResponseEntity.status(403).body(null); // Forbidden for non-Admin users
+//        }
+//
+//        // Call the service layer to handle course creation
+//        CourseEntity createdCourse = courseService.addCourse(course);
+//        return ResponseEntity.ok(createdCourse);
+//    }
 }
--- LMS/src/main/java/org/software_assignment/lms/service/CourseService.java
+++ LMS/src/main/java/org/software_assignment/lms/service/CourseService.java
@@ -1,4 +1,24 @@
 package org.software_assignment.lms.service;
+import org.software_assignment.lms.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.software_assignment.lms.entity.CourseEntity;
 
+@Service
 public class CourseService {
+//    @Autowired
+//    private CourseRepository courseRepository;
+//
+//    public CourseService(CourseRepository courseRepository) {
+//        this.courseRepository = courseRepository;
+//    }
+//
+//    public CourseEntity addCourse(CourseEntity course) {
+//        if (course == null) {
+//            throw new IllegalArgumentException("Course cannot be null");
+//        }else{
+//             course.setDuration(0);
+//        }
+//        return courseRepository.save(course);
+//    }
 }
