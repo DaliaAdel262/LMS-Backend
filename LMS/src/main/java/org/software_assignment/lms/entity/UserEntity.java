0e7c9af6ad32739add386b77fe6a435aaadd4e6f
Salma884 <147331892+Salma884@users.noreply.github.com>
2024-12-14 23:58:24 +0200

    Update UserEntity.java

--- LMS/src/main/java/org/software_assignment/lms/entity/UserEntity.java
+++ LMS/src/main/java/org/software_assignment/lms/entity/UserEntity.java
@@ -1,4 +1,28 @@
 package org.software_assignment.lms.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.ArrayList;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class UserEntity {
+    @Id
+    private int id;
+    private String name;
+    private String birthday;
+    private String email;
+    private String password;
+
 }
+
+
+
