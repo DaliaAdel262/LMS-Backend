771b430e7cb99b0af9bcf943dedf5435a2925e87
Dalia Adel <daliaadel262@gmail.com>
2024-12-19 14:15:22 +0200

    removed comment

--- LMS/src/main/java/org/software_assignment/lms/controller/CourseController.java
+++ LMS/src/main/java/org/software_assignment/lms/controller/CourseController.java
@@ -60,24 +60,7 @@ public class CourseController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found: " + e.getMessage());
         }
     }
-
-    // @DeleteMapping("/{id}")
-    // public ResponseEntity<String> deleteCourse(
-    //         @PathVariable String id,
-    //         @RequestHeader("role") String userRole
-    // ) {
-    //     // Verify the user's role in the controller
-    //     if (!"Admin".equalsIgnoreCase(userRole)) {
-    //         return ResponseEntity.status(403).body(null); // Forbidden for non-Admin users
-    //     }
-    //     try {
-    //         courseService.deleteCourseById(id);
-    //         return ResponseEntity.ok("Course deleted successfully!");
-    //     } catch (Exception e) {
-    //         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found: " + e.getMessage());
-    //     }
-    // }
-
+    
     @GetMapping(value = "/{id}")
     public ResponseEntity<CourseEntity> getCourseById(@PathVariable String id) {
         try {
