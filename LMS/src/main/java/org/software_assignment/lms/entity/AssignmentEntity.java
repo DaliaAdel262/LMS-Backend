e03cc047c6c51dde994edec28b311fe4094af1d3
Dalia Adel <daliaadel262@gmail.com>
2024-12-19 14:06:50 +0200

    assignment functions finished

--- LMS/src/main/java/org/software_assignment/lms/entity/AssignmentEntity.java
+++ LMS/src/main/java/org/software_assignment/lms/entity/AssignmentEntity.java
@@ -7,17 +7,19 @@ public class AssignmentEntity {
     private int assignmentID;
     private String assignmentName;
     private String assignmentDescription;
+    private String filePath;
     private String dueDate;
     private String courseId;
     Map<Integer, String> studentSubmissions;
     public AssignmentEntity() {}
 
-    public AssignmentEntity(int assignmentID, String assignmentName, String assignmentDescription, String dueDate, String courseId) {
+    public AssignmentEntity(int assignmentID, String assignmentName, String assignmentDescription, String dueDate, String filePath, String courseId) {
         this.assignmentID = assignmentID;
         this.assignmentName = assignmentName;
         this.assignmentDescription = assignmentDescription;
         this.dueDate = dueDate;
         this.courseId = courseId;
+        this.filePath = filePath;
         this.studentSubmissions = new HashMap<>();
     }
 
@@ -61,7 +63,19 @@ public class AssignmentEntity {
         this.courseId = courseId;
     }
 
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
     public Map<Integer,String> getStudentSubmission(){
         return this.studentSubmissions;
+    }
+
+    public void addSubmission(int student_id, String filePath){
+        this.studentSubmissions.put(student_id, filePath);
     }
 }
--- LMS/src/main/java/org/software_assignment/lms/service/AssignmentService.java
+++ LMS/src/main/java/org/software_assignment/lms/service/AssignmentService.java
@@ -4,16 +4,66 @@ import org.software_assignment.lms.repository.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @Service
 public class AssignmentService {
-    
-    public int gradeAssignment(int studentId){
 
+    @Autowired
+    private AssignmentRepo assignmentRepo;
+
+    public AssignmentService(AssignmentRepo assignmentRepo){
+        this.assignmentRepo = assignmentRepo;
     }
 
-    public void addStudentSubmission(int studentId, String submissionPath){
-        
+    public void addStudentSubmission(int studentId, int assignmentID, String submissionPath){
+        AssignmentEntity assignment = assignmentRepo.findById(assignmentID);
+        if(assignment==null){
+
+            throw new NoSuchElementException("Assignment not found");
+
+        }else{
+
+            String dueDateString = assignment.getDueDate(); 
+            LocalDate dueDate = LocalDate.parse(dueDateString, DateTimeFormatter.ISO_LOCAL_DATE);
+            LocalDate currentDate = LocalDate.now();
+            
+            if (currentDate.isAfter(dueDate)) {
+                throw new IllegalArgumentException("Cannot submit the assignment. The due date has passed.");
+            }
+            Map<Integer,String> studentSubmission = assignment.getStudentSubmission();
+            if (studentSubmission.containsKey(studentId)) {
+                studentSubmission.put(studentId, submissionPath);
+            } else {
+                assignment.addSubmission(studentId, submissionPath);
+            }
+            assignmentRepo.save(assignment);
+            
+        }
+    }
+    
+    public String gradeAssignment(int studentId, int assignmentID){
+        AssignmentEntity assignment = assignmentRepo.findById(assignmentID);
+        if(assignment==null){
+            throw new NoSuchElementException("Assignment not found");
+        }else{
+            Map<Integer,String> studenSubmission = assignment.getStudentSubmission();
+            if(studenSubmission.containsKey(studentId)){
+                return studenSubmission.get(studentId);
+            }else{
+                return "Student hasn't submitted assignment";
+            }
+        }
+    }
+
+    public String getAssignmentFilePath(int assignmentID) {
+        AssignmentEntity assignment = assignmentRepo.findById(assignmentID);
+        if (assignment == null) {
+            throw new NoSuchElementException("Assignment not found");
+        }
+        return assignment.getFilePath();
     }
 }
