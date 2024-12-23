f78441a5f285e35fd8939c46a56f389ce1544aef
omar-dev0 <11410120211067@stud.cu.edu.eg>
2024-12-23 13:26:55 +0200

    add some imports

--- LMS/src/main/java/org/software_assignment/lms/service/CourseService.java
+++ LMS/src/main/java/org/software_assignment/lms/service/CourseService.java
@@ -1,8 +1,8 @@
 package org.software_assignment.lms.service;
 
-import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
